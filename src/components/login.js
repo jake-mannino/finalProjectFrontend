f67f2e44802b172cce1b9633486f9661175e924b
jake-mannino <yomannino@gmail.com>
2021-05-05 15:59:13 -0400

    added more pseudocode starting on login, register, header, nav

--- src/components/login.js
+++ src/components/login.js
@@ -1,3 +1,18 @@
+        
+        //import nav, // link to register.js
+                // anonymous version for unlogged / unregisters users 
+                // just "sign -up" on nav, if clicked links to sign up page, 
+                // nav button switches to login on register.js
+//body
+    //container
+            //div
+                //row
+                    //col
+                        //card
+                            //subheader or h1
+                                //two input fields with :valid classes if valid
+                                    //email
+                                    //password
         // <div> row
         //     <div> col
         //         <form>
--- src/components/register.js
+++ src/components/register.js
@@ -1,1 +1,19 @@
-// link to login.js
+//import nav, // link to login.js
+    //anonymous version for unlogged / unregisters users,
+    // just "login" on nav, if clicked links to login page, this nav button switches to sign on login.js
+
+//body
+    //container
+            //div
+                //row
+                    //col
+                        //card
+                            //subheader or h1
+                                //two input fields with :valid classes if valid
+                                    //email
+                                    //password
+
+
+//export
+
+
