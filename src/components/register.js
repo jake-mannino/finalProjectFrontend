17a6ce9eeb65cd79b7d39bc542258fd8bf0920c9
jake-mannino <yomannino@gmail.com>
2021-05-06 15:27:16 -0400

    debug

--- src/components/register.js
+++ src/components/register.js
@@ -1,5 +1,4 @@
-
-import React { useState } from 'react';
+import React, { useState } from 'react';
 // import Auth from 'Auth.js';
 import axios from 'axios'
 export default function Register(props) {
