e8a52c203f7329f9e00cf5c40d4d71ee4c992200
jake-mannino <yomannino@gmail.com>
2021-05-06 12:29:18 -0400

    app.css had wrong path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-import '/App.css';
+import './App.css';
 import Navi from './components/Navi';
 import bootstrap from 'bootstrap';
 
