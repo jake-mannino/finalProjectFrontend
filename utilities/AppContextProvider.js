e072633de89b5f81e9d0ac85ea61ae33b7c4c3eb
jake-mannino <yomannino@gmail.com>
2021-05-10 10:04:29 -0400

    change pseudo class :hover to :not(:hover)

---
+++ utilities/AppContextProvider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppContext = React.createContext.Context({});
+
+export const AppProvider = AppContext.Provider;
+
+export default function AppContext() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
